8c352f4d3ee723fad773ab363ce6b0284a0d3344
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-10-12 19:26:51 +0000

    Deploy vberger/token_store to github.com/vberger/token_store.git:gh-pages

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["token_store"] = ["impl&lt;V&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"token_store/struct.Token.html\" title=\"struct token_store::Token\">Token</a>&lt;V&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["token_store"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;'a mut <a class=\"struct\" href=\"token_store/struct.Store.html\" title=\"struct token_store::Store\">Store</a>&gt; for <a class=\"struct\" href=\"token_store/struct.StoreProxy.html\" title=\"struct token_store::StoreProxy\">StoreProxy</a>&lt;'a&gt;","impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;&amp;'a mut <a class=\"struct\" href=\"token_store/struct.StoreProxy.html\" title=\"struct token_store::StoreProxy\">StoreProxy</a>&lt;'b&gt;&gt; for <a class=\"struct\" href=\"token_store/struct.StoreProxy.html\" title=\"struct token_store::StoreProxy\">StoreProxy</a>&lt;'a&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;'b: 'a,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
